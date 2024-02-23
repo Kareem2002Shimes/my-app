9618508f20d623322d78e577f4e68266fa19c434
kareem <kareem4shimas@gmail.com>
2024-02-23 17:11:52 +0200

    Update Header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,13 +3,13 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 function Header() {
-  const links = [
-    { url: '/home', title: 'Home' },
-    { url: '/about', title: 'About' },
-    { url: '/blog', title: 'Blog' },
-    { url: '/contact', title: 'Contact' },
-    { url: '/app', title: 'App' },
-  ];
+  // const links = [
+  //   { url: '/home', title: 'Home' },
+  //   { url: '/about', title: 'About' },
+  //   { url: '/blog', title: 'Blog' },
+  //   { url: '/contact', title: 'Contact' },
+  //   { url: '/app', title: 'App' },
+  // ];
   return (
     <Navbar expand='md' className='bg-body-tertiary'>
       <Container>
@@ -17,13 +17,13 @@ function Header() {
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav'>
           <Nav className='me-auto'>
-            {links.map((link, i) => {
+            {/* {links.map((link, i) => {
               return (
                 <Nav.Link key={i} href={link.url}>
                   {link.title}
                 </Nav.Link>
               );
-            })}
+            })} */}
             <NavDropdown title='Dropdown' id='basic-nav-dropdown'>
               <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
               <NavDropdown.Item href='#action/3.2'>
